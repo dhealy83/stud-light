0751819c50442fa6e124bb22ba4e61a4a584bbf1
myang5t3r <matthew.yang@hunterdouglas.com>
2022-10-14 17:50:50 -0600

    Able to create a user and login this auth working

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,8 +1,10 @@
 const db = require("../config/connection");
 
 const { User, Collection, Card } = require("../models");
+const { collection } = require("../models/Card");
 
 const userSeeds = require("./userSeed.json");
+const cardSeeds = require("./cardSeed.json");
 
 db.once("open", async () => {
   try {
@@ -13,18 +15,24 @@ db.once("open", async () => {
     // Create collection
     // update user so collection is attached to them
 
+    // for (const i of cardSeeds) {
+    //   console.log(i);
+    //   const currentCard = await Card.create("cards"[{}]);
+    // }
+
     // for of used for arrays
-    for (const i of userSeeds) {
-      // const currentCard = await Card.create(i.cards)
-      const currentCollection = await Collection.create(i.collections);
-      const collectionID = currentCollection.map((obj) => obj._id);
-      await User.create({
-        username: i.username,
-        email: i.email,
-        password: i.password,
-        collections: collectionID,
-      });
-    }
+    // for (const i of userSeeds) {
+    //   const currentCard = await Card.create(i.cards);
+    //   collectionCard = currentCard.map((obj) => obj._id);
+    //   const currentCollection = await Collection.create(i.collections);
+    //   const collectionID = currentCollection.map((obj) => obj._id);
+    //   await User.create({
+    //     username: i.username,
+    //     email: i.email,
+    //     password: i.password,
+    //     collections: { collectionID, cards: [collectionCard] },
+    //   });
+    // }
 
     console.log("All seeded");
     process.exit(0);
