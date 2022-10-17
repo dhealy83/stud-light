cbd74627b5028a03f283e845f3d79bbe25364939
David Healy <drtjprdave@gmail.com>
2022-10-17 01:48:32 -0600

    Navbar now taking you to collections and cards.

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useMutation, useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
 
 import { Link } from "react-router-dom";
 import Modal from "react-bootstrap/Modal";
@@ -17,11 +17,8 @@ import OffcanvasNav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 
 import {
-  Form,
-  ToggleButton,
   Button,
   Col,
-  Row,
   Container,
   Carousel,
   Card,
@@ -30,12 +27,17 @@ import {
 // All of the image imports
 import cardBackground from "../../assets/cardBackground.jpg";
 // import CardData from "./CardData";
+
 // Local css files
 import "./FlashCard.css";
 import RadioButtons from "./RadioButtons";
 
-const FlashCarousel = () => {
+const FlashCarousel = ({ route, navigation }) => {
+  //const { currentIndex } = route.params;
+
+  const collectionId = localStorage.getItem("currentCollection");
   const [index, setIndex] = useState(0);
+  const [collectionID, setCollectionID] = useState(null);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
@@ -48,8 +50,6 @@ const FlashCarousel = () => {
 
   const [toggle, setToggle] = useState(false);
 
-  const collectionId = localStorage.getItem("currentCollection");
-
   const { loading, data, error } = useQuery(
     QUERY_SINGLE_COLLECTION,
     {
@@ -57,132 +57,142 @@ const FlashCarousel = () => {
     },
     []
   );
+
+  // useEffect(() => {
+  //   if (toggle) window.location.reload();
+  // }, [toggle]);
+
   if (loading) return "Loading";
-  // console.log(data);
-  const collectionTitle = data.collection.title;
+
+  const collectionTitle = data.collection.title ?? "";
+
+  const collectionNotes = data.collection.notes ?? "";
 
   return (
     <>
-      <OffcanvasNav />
-      <div className="wholeCard h-100">
-        <div>
-          <Carousel
-            activeIndex={index}
-            onSelect={handleSelect}
-            className="card m-2 border boarder-2 boarder-secondary rounded-3"
-            interval="10000000"
-          >
-            {data.collection.cards.map((obj, i) => {
-              return (
-                <Carousel.Item className="buttonCheckBox" key={obj._id}>
-                  <RadioButtons key={i} index={i} />
-                  <Col className="d-flex justify-content-evenly mx-3">
-                    <h1>{collectionTitle}</h1>
-                    <ButtonGroup className="m-2 d-flex justify-content-end"></ButtonGroup>
-                  </Col>
-                  <img
-                    className="d-block w-100"
-                    src={cardBackground}
-                    alt="First slide"
-                  />
-                  <Carousel.Caption className="text-dark">
-                    <Container>
-                      <Col>
-                        <Row>
-                          {toggle ? (
-                            <a href="#" onClick={() => setToggle(!toggle)}>
-                              <CardIcon icon={<Question size="50" />} />
-                            </a>
-                          ) : (
-                            <a href="#" onClick={() => setToggle(!toggle)}>
-                              <CardIcon icon={<Answer size="50" />} />
-                            </a>
-                          )}
-
-                          {/* <a href="#" onClick={() => setToggle(!toggle)}>
-                 <CardIcon icon={<Question size="50" />} />
-                </a> */}
-                          {toggle && (
-                            <div className="flip-horizontal-bottom">
-                              <div class="card">
-                                <div class="card-body">
-                                  <p class="card-text">{obj.answer}</p>
-                                </div>
-                              </div>
-                              <p className="text-success">"Flipped"</p>
-                            </div>
-                          )}
-                          {!toggle && (
-                            <div className="flip-horizontal-bottom">
-                              <div class="card">
-                                <div class="card-body">
-                                  <p class="card-text">{obj.question}</p>
-                                </div>
-                              </div>
-                              <p className="text-info">"Not Flipped"</p>
-                            </div>
-                          )}
-                        </Row>
-                      </Col>
-                    </Container>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              );
-            })}
-          </Carousel>
-        </div>
-        <div className="d-flex">
-          <div className="col v-100">
-            <Card className="m-2">
-              <Card.Title className="bg-secondary text-white p-2 rounded-top">
-                Notes
-              </Card.Title>
-              <Card.Body>
-                {/* notes are currently hardcoded to grab the notes of the second card in the array */}
-                <Card.Text>{data.collection.cards[0].notes}</Card.Text>
-              </Card.Body>
-            </Card>
-
-            <div className="m-2">
-              <Link to="/UpdateCard">
-                <Button variant="secondary" size="lg" className="w-100">
-                  Update
-                </Button>
-              </Link>
-            </div>
-            <div className="m-2">
-              <Button
-                variant="secondary"
-                size="lg"
-                className="w-100"
-                onClick={handleShow}
+      <OffcanvasNav setCollectionID={(id) => setCollectionID(id)} />
+      <div className="d-flex justify-content-center h-75">
+        <div className="wholeCard">
+          <div>
+            {data.collection.cards.length ? (
+              <Carousel
+                activeIndex={index}
+                onSelect={handleSelect}
+                className="card m-2 border boarder-2 boarder-secondary rounded-3 "
+                interval="10000000"
               >
-                Delete Card
-              </Button>
-            </div>
-            <div className="m-2 mt-auto">
-              <Link to="/AddCard">
-                <Button variant="secondary" size="lg" className="w-100">
-                  Add A New Card
+                {data.collection.cards.map((obj, i) => {
+                  return (
+                    <Carousel.Item className="buttonCheckBox " key={obj._id}>
+                      <RadioButtons key={i} index={i} />
+                      <Col className="d-flex justify-content-center mx-3">
+                        <h1>{collectionTitle}</h1>
+                        <ButtonGroup className="m-2 d-flex justify-content-end"></ButtonGroup>
+                      </Col>
+                      <img
+                        className="d-block w-100"
+                        src={cardBackground}
+                        alt="First slide"
+                      />
+                      <Carousel.Caption className="carouselText text-dark ">
+                        <Container className="">
+                          <Col className="">
+                            {toggle ? (
+                              <a href="#" onClick={() => setToggle(!toggle)}>
+                                <CardIcon icon={<Question size="30" />} />
+                              </a>
+                            ) : (
+                              <a href="#" onClick={() => setToggle(!toggle)}>
+                                <CardIcon icon={<Answer size="30" />} />
+                              </a>
+                            )}
+
+                            {toggle && (
+                              <div className="flip-horizontal-bottom ">
+                                <div className="">
+                                  <p className=" card-text">{obj.answer}</p>
+                                </div>
+                              </div>
+                            )}
+                            {!toggle && (
+                              <div className="flip-horizontal-bottom">
+                                <div className="">
+                                  <p className=" card-text">{obj.question}</p>
+                                </div>
+                              </div>
+                            )}
+                          </Col>
+                        </Container>
+                      </Carousel.Caption>
+                      <div>
+                        <Card className="m-2">
+                          <Card.Title className="bg-secondary text-white p-2 rounded-top">
+                            Notes
+                          </Card.Title>
+                          <Card.Body>
+                            {/* notes are currently hardcoded to grab the notes of the second card in the array */}
+                            <Card.Text>
+                              {/* This should be obj.notes */}
+
+                              {/* // ? data.collection.cards[0].notes // : "" */}
+                              <div>{obj.notes}</div>
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </div>
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
+            ) : (
+              <div>Empty Cards</div>
+            )}
+          </div>
+          <div className="d-flex">
+            <div className="col v-100">
+              <div className="m-2">
+                <Link to="/UpdateCard">
+                  <Button variant="secondary" size="lg" className="w-100">
+                    Update
+                  </Button>
+                </Link>
+              </div>
+              <div className="m-2">
+                <Button
+                  variant="secondary"
+                  size="lg"
+                  className="w-100"
+                  onClick={handleShow}
+                >
+                  Delete Collection
                 </Button>
-              </Link>
+              </div>
+              <div className="m-2 mt-auto">
+                <Link to="/AddCard">
+                  <Button variant="secondary" size="lg" className="w-100">
+                    Add A New Card
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Are you sure you'd like to delete this card?
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancel
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Delete Card
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Are you sure you'd like to delete this card?</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Delete Card
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </div>
       <Footer />
     </>
